bff4630ef58a9e9efc6ef0234cffb3a100d986da
seungupnoh <soungho70_@naver.com>
2020-12-21 16:54:07 +0900

    test code

--- backend/gitfolio/src/test/java/com/gitfolio/GitfolioApplicationTests.java
+++ backend/gitfolio/src/test/java/com/gitfolio/GitfolioApplicationTests.java
@@ -1,16 +1,59 @@
 package com.gitfolio;
 
+import com.gitfolio.user.Member;
+import com.gitfolio.user.MemberRepository;
+import com.gitfolio.user.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class GitfolioApplicationTests
 {
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    final String principalName = "!231231";
+    public long id = 1L;
 
     @Test
     void contextLoads()
     {
-
     }
 
+    @Test
+    void duplicatedJoinMember() {
+        Member member = new Member();
+        member.setId(id++);
+        member.setPrincipalName(principalName);
+        assert memberService.joinMember(member);
+
+        member.setId(id++);
+        assert !memberService.joinMember(member);
+    }
+
+    @Test
+    void JoinSearchUpdate() {
+        Member member = new Member();
+        member.setId(id++);
+        member.setPrincipalName(principalName);
+        assert memberService.joinMember(member);
+
+        Optional<Member> op_member = memberRepository.findByPrincipalName(principalName);
+        assert op_member.isPresent();
+
+        Member member1 = op_member.get();
+        String gname = "123";
+        member1.setGithubName(gname);
+        assert memberService.updateMember(member1);
+
+        Optional<Member> member2 = memberRepository.findByPrincipalName(principalName);
+        assert member2.isPresent();
+        assert member2.get().getGithubName().equals(gname);
+    }
 }
